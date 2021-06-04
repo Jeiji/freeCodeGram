dd8bddceade1405878c0a4883b4be1a90113fc05
jeiji <jeiji@me.com>
2021-06-04 17:37:02 +0900

    Adding laravel/ui and bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
 const mix = require('laravel-mix');
-
 
 /*
  |--------------------------------------------------------------------------
@@ -8,22 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]).browserSync({
-        files: [
-            "resources/views/**/*.*.*",
-            "public/**/*.*"
-        ],
-        proxy: {
-            target: process.env.APP_DEV_URL
-        }
-    });
-
-
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
